5cdc4ee311fadad10dfb97e2c4587a1f45e63577
xiolest1 <35781301+xiolest1@users.noreply.github.com>
2018-02-12 09:48:22 -0500

    Create Main.cpp
    This will be the main.cpp file for group project

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+//courseID:CIS165-001
+//GroupLeader: Joan
+//GroupProgrammers:
+//Andre
+//Ivana
+//James
+//Tyler
+//Prof. Wang
+//Assignment#1
+//Due by 5/24/2012
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
